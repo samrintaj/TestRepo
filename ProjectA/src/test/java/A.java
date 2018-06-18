35f24044863baef5f846e4d5862aa399337babc9
Ajay NK <ajay.thirumalesh@gmail.com>
2018-06-18 19:31:12 +0530

    Adding another class file changes done in existing file

--- ProjectA/src/test/java/A.java
+++ ProjectA/src/test/java/A.java
@@ -4,6 +4,9 @@ public class A {
 	public static void main(String[] args) {
 
 		System.out.println("Simple hello world");
+		System.out.println("New Commit");
+		int i=10;
+		System.out.println(i);
 
 	}
 
---
+++ ProjectA/src/test/java/B.java
@@ -0,0 +1,10 @@
+
+public class B {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hi");
+
+	}
+
+}
